8e9f259538225811e3a8acceac1c10d01dad7be5
Jackie Lin <jlin00@stuy.edu>
2020-06-03 16:24:18 -0400

    added routes and templates for login

--- app/__init__.py
+++ app/__init__.py
@@ -40,10 +40,41 @@ def no_login_required(f):
     return dec
 
 #====================================================
+# LOGIN FUNCTIONS
 
 @app.route("/")
 def root():
-    return "ROOT PAGE"
+    '''def root(): Redirects to login page if not in session, redirects to home if in session'''
+    if 'username' in session: #if logged in
+        return redirect('/daily')
+    return redirect('/login')
+
+@app.route("/login")
+@no_login_required
+def login():
+    '''def login(): login requirement'''
+    return render_template('login.html', isLogin=True, login="active")
+
+@app.route("/auth")
+@no_login_required
+def auth():
+    return "HELLO WORLD"
+
+@app.route("/signup")
+@no_login_required
+def signup():
+    '''def signup(): sign up route, takes in a form for signing up'''
+    return render_template("signup.html", isLogin=True, signup="active")
+
+@app.route("/signupcheck")
+@no_login_required
+def signupcheck():
+    return "HELLO WORLD"
+
+#====================================================
+# STARTING HERE, USER MUST BE LOGGED IN
+
+#====================================================
 
 if __name__ == "__main__":
     app.debug = True
